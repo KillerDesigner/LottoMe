81eccfd3a1d0089564598af550571758933a0082
KillerDesigner <git@killerdesigner.com>
2013-11-26 13:31:22 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LottoMe::Application.config.secret_token = '9e90c9e42623401dd1fba5507276075d1927c9e9197dacd1e6da4639b41309a1a88e3efd3e6808a3c80e36b91178cb7b4188a630191c0e55850fb6c71218eed1'
